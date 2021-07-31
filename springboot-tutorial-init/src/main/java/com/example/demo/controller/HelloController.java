57aac44e19e34ae215cc5dac37310baabe6809ac
kyujinn <heather0220@naver.com>
2021-07-31 12:20:55 +0900

    실습

--- springboot-tutorial-init/src/main/java/com/example/demo/controller/HelloController.java
+++ springboot-tutorial-init/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.empty.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -11,4 +16,29 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+    // return : Hello yujin
+
+    @GetMapping("hello-string")
+    @ResponseBody   // 전환이 되서 나감
+    public String helloString(String name) {
+        return String.format("Hello, %s", name);
+    }
+    // return : Hello, yujin
+
+    @GetMapping("hello-map")
+    @ResponseBody
+    public Map<String,Object> helloMap(String name) {
+        Map<String,Object> m = new HashMap<>();   // 키,value
+        m.put("name", name);
+        return m;
+    }
+    // return : {"name":"yujin"}  -> json 형태
+
+    @GetMapping("hello-object")
+    @ResponseBody
+    public Hello helloObject(String name) {
+        Hello h = new Hello(name);
+        return h;
+    }
+    // return : {"name":"yujin"}  -> json 형태
 }
---
+++ springboot-tutorial-init/src/main/java/com/example/demo/empty/Hello.java
@@ -0,0 +1,8 @@
+package com.example.demo.empty;
+
+import lombok.Data;
+
+@Data
+public class Hello {
+    final String name;
+}
